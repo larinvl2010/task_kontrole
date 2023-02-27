b4e7051b49a4ecbef8e0fc722ba983cae088ffe3
Vladimir <larinvl2010@mail.ru>
2023-02-27 21:06:48 +0300

    Выполнено контольное задание

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter a number of elements array");
+int N =Convert.ToInt32(Console.ReadLine());
+string[] array=new string[N];
+Console.WriteLine("Enter strings");
+for (int i = 0; i<array.GetLength(0); i++)
+    {
+        array[i]=Console.ReadLine();
+    }
+PrintArray(array);
+Console.WriteLine("--------------------");
+SelectionArray(array);
+void PrintArray(string [] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        
+            Console.Write($"{array[i]} ");
+    
+    }
+Console.WriteLine();
+}
+
+void SelectionArray(string [] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        if (array[i].Length<=3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    
+    }
+}
